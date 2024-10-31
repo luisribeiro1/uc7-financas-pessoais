bccd2a6d558c67e4009f07c0e811bf102b27c38a
LETICIA MARIA SILVA RAMOS DE OLIVEIRA <leticia.msroliveira@senacsp.edu.br>
2024-10-31 19:46:38 -0300

    31/10/2024: FinançasPessoais|Etapa 1

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+session_start();
+
+$requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
+
+$requisicao = str_replace("/uc7/financeiro/", "", $requisicao);
+
+$segmentos = explode("/",$requisicao);
+
+$controlador = isset($segmentos[0]) ? $segmentos[0] : "financeiro-adm";
+$metodo = isset($segmentos[1]) ? $segmentos[1] : "index";
+$identificador = isset($segmentos[2]) ? $segmentos[2] : null;
+
+switch($controlador){
+    case "financeiro-adm":
+        require "controllers/FinanceiroController.php";
+        $controller = new FinanceiroController();
+        break;
+    default:
+        echo "Página não encontrada";
+        break;
+}
+
+if ($identificador){
+    $controller->$metodo($identificador);
+}else{
+    $controller->$metodo();
+}
