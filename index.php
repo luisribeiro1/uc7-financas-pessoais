df9f2560cf0e79d09350bd7fcf227594ef31232a
ERICK SILVA SOUZA DO PRADO <erick.ssprado@senacsp.edu.br>
2024-10-31 19:47:56 -0300

    FinançasPessoais - 31/10/2024 - Etapa 1

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+$requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
+
+$requisicao = str_replace("/uc8/financas/","",$requisicao);
+
+$segmentos = explode("/",$requisicao);
+
+$controlador = isset($segmentos[0]) ? $segmentos[0] : "financeiro";
+$metodo = isset($segmentos[1]) ? $segmentos[1] : "index";
+$identificador = isset($segmentos[2]) ? $segmentos[2] : null ;
+
+switch($controlador){
+
+    case "financeiro":
+        require "controllers/FinanceiroController.php";
+        $controller = new FinanceiroController();
+        break;
+
+    default:
+
+    echo "pagina não encontrada";
+    break;
+
+}
+
+if($identificador){
+
+    $controller->$metodo($identificador);
+}else{
+
+    $controller->$metodo;
+}
