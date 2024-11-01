77248775e3910dd28d74d41d5d0958dc8825b776
PAULO HENRIQUE BARROS MOREIRA <paulo.hbmoreira@senacsp.edu.br>
2024-10-31 21:42:56 -0300

    FinaçasPessoais - 31/10/2024 - Etapa 2

--- controllers/FinanceiroController.php
+++ controllers/FinanceiroController.php
@@ -4,4 +4,26 @@ require_once "models/FinanceiroModel.php";
 
 class FinanceiroController{
 
+    private $url = "http://localhost/financas_pessoais";
+    private  $financeiroModel;
+
+    public function index(){
+
+    }
+
+    public function criar(){
+        echo "método criar() foi chamado";
+        $acao = "criar";
+    }
+
+    public function atualizar(){
+        echo "método atualizar() foi chamado";
+        $acao = "atualizar";
+    }
+
+    public function cancelar(){
+        echo "método cancelar() foi chamado";
+        $acao = "cancelar";
+    }
+
 }
--- index.php
+++ index.php
@@ -3,7 +3,8 @@ session_start();
 
 $requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
 
-$requisicao = str_replace("financas_pessoais/","",$requisicao);
+$requisicao = str_replace("/teste/financas_pessoais/","",$requisicao);
+echo $requisicao;
 
 $segmentos = explode("/",$requisicao);
 
@@ -12,13 +13,16 @@ $metodo = isset($segmentos[1]) ? $segmentos[1] : "index";
 $identificador = isset($segmentos[2]) ? $segmentos[2] : null;
 
 switch($controlador){
-    case "finceiro-adm":
+    case "financeiro-adm":
         require "controllers/FinanceiroController.php";
         $controller = new FinanceiroController();
         break;
         default:
         echo "Pagina não encontrada";
+        echo "requisicao $requisicao, controlador $contorlador, metodo $metodo, identificador $identificador";
         break;
+      
+
     
 }
 if ($identificador){
@@ -26,6 +30,3 @@ if ($identificador){
 }else{  
     $controller->$metodo();
 }
-
-
-
