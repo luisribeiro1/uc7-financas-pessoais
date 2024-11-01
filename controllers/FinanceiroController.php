2ef93353bafffd2e60f6c51e899a6b74b29ed75c
LETICIA MARIA SILVA RAMOS DE OLIVEIRA <leticia.msroliveira@senacsp.edu.br>
2024-10-31 21:34:40 -0300

    31/10/2024 - FinançasPessoais| Etapa 2

---
+++ controllers/FinanceiroController.php
@@ -0,0 +1,66 @@
+<?php
+
+require_once "models/FinanceiroModel.php";
+
+class FinanceiroController{
+    private $baseUrl = "https://localhost/uc7/financas-pessoais";
+
+    private $financeiroModel;
+
+    public function __construct(){
+        $this->financeiroModel = new Financeiro();
+    }
+
+    public function index(){
+        $lista_de_financas = $this->financeiroModel->getAllFinancas();
+
+        $baseUrl = $this->baseUrl;
+
+        require "views/FinanceiroList.php";
+    }
+
+    public function criar(){
+        $baseUrl = $this->baseUrl;
+        $acao = "criar";
+        require "views/FinanceiroForm.php";
+
+    }
+
+    public function editar($id){
+        $financeiro = $this->financeiroModel->getById($id)
+        $data = $financeiro["data"];
+        $descricao = $financeiro["descricao"];
+        $valor = $financeiro["valor"];
+        $deb_cred = $financeiro["deb_cred"];
+        $status = $financeiro["status"];
+
+        $baseUrl = $this->baseUrl;
+
+        $acao = "editar";
+        require "views/FinanceiroForm.php";
+    }
+  
+    public function atualizar(){
+        $data = $_POST["data"];
+        $descricao = $_POST["descricao"];
+        $valor = $_POST["valor"];
+        $deb_cred = $_POST["deb_cred"];
+        $status = $_POST["status"];
+
+        $acao = $_POST["acao"];
+
+        if($acao=="editar"){
+            $id = $_POST["id_financeiro"];
+            $this->financeiroModel->update($id,$data,$descricao,$valor,$deb_cred,$status);
+        }else{
+            $this->cardapioModel->insert($data,$descricao,$valor,$deb_cred,$status);
+        }
+        header("location :".$this->baseUrl."/financeiro");
+    }
+
+    public function cancelar($id){
+        $this->financeiroModel->delete($id);
+
+        header("location: ".$this->baseUrl."/financeiro");
+    }
+}
--- index.php
+++ index.php
@@ -4,16 +4,16 @@ session_start();
 
 $requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
 
-$requisicao = str_replace("/uc7/financeiro/", "", $requisicao);
+$requisicao = str_replace("/uc7/financeiro", "", $requisicao);
 
 $segmentos = explode("/",$requisicao);
 
-$controlador = isset($segmentos[0]) ? $segmentos[0] : "financeiro-adm";
+$controlador = isset($segmentos[0]) ? $segmentos[0] : "financeiro";
 $metodo = isset($segmentos[1]) ? $segmentos[1] : "index";
 $identificador = isset($segmentos[2]) ? $segmentos[2] : null;
 
 switch($controlador){
-    case "financeiro-adm":
+    case "financeiro":
         require "controllers/FinanceiroController.php";
         $controller = new FinanceiroController();
         break;
@@ -22,8 +22,8 @@ switch($controlador){
         break;
 }
 
-if ($identificador){
-    $controller->$metodo($identificador);
-}else{
-    $controller->$metodo();
-}
+// if ($identificador){
+//     $controller->$metodo($identificador);
+// }else{
+//     $controller->$metodo();
+// }
