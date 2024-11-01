1d9a8adeb58280f538f306144585a9c2fac87086
Luis Henrique <luis.h.souza@outlook.com.br>
2024-10-31 21:16:36 -0300

    Finanças Pessoais - 31/10/24: Etapa 02

--- controllers/FinanceiroController.php
+++ controllers/FinanceiroController.php
@@ -1,1 +1,35 @@
 <?php
+
+class FinanceiroController 
+{
+
+  public $baseUrl = "http://localhost/uc7/financas_pessoais";
+
+  private $FinanceiroModel;
+
+  public $acao;
+
+  public function index() {
+    echo "Método index() foi chamado!";
+  }
+
+  public function criar() {
+    $acao = "criar";
+    echo "Método criar() foi chamado!";
+  }
+
+  public function atualizar() {
+    $acao = "atualizar";
+    echo "Método atualizar() foi chamado!";
+  }
+
+  public function cancelar() {
+    $acao = "cancelar";
+    echo "Método cancelar() foi chamado";
+  }
+
+
+
+
+
+}
--- index.php
+++ index.php
@@ -4,29 +4,29 @@ session_start();
 
 $requisicao = trim(strtolower($_SERVER['REQUEST_URI']));
 
-$requisicao = str_replace("/uc7/finanças-pessoais/", "", $requisicao);
+$requisicao = str_replace("/uc7/financas_pessoais/", "", $requisicao);
 
 $segmentos = explode("/", $requisicao);
 
-$controlador = isset($segmentos[0]) ? $seguimentos[0] : "finaceiro";
-$metodo = isset($segmentos[1]) ? $seguimentos[1] : "index";
-$identificador = isset($segmentos[2]) ? $seguimentos[2] : null;
+$controlador = isset($segmentos[0]) ? $segmentos[0] : "financeiro";
+$metodo = isset($segmentos[1]) ? $segmentos[1] : "index";
+$identificador = isset($segmentos[2]) ? $segmentos[2] : null;
 
 switch ($controlador) {
   
-  case 'finaceiro';
+  case 'finaceiro':
     require "controllers/FinanceiroController.php";
-    // $controller = new FinanceiroController();
+    $controller = new FinanceiroController();
     break;
 
     default:
-    $baseUrl = "http://localhost/uc7/financeiro";
-    header("location: " . $baseUrl . "/views/pagina-404.html");
+    $baseUrl = "http://localhost/uc7/financas_pessoais";
+    // header("location: " . $baseUrl . "/views/pagina-404.html");
     break;
 }
 
-if ($identificador) {
-  $controller->$metodo($identificador);
-} else {
-  $controller->$metodo();
-}
+// if ($identificador) {
+//   $controller->$metodo($identificador);
+// } else {
+//   $controller->$metodo();
+// }
