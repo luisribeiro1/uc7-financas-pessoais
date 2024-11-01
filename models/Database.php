4baf9bd0e12fdf5eb511d1ade1c30eb6d66db423
Luis Henrique <luis.h.souza@outlook.com.br>
2024-10-31 22:22:47 -0300

    Finanças Pessoais - 31/10/24: Etapa 04

--- models/Database.php
+++ models/Database.php
@@ -1,1 +1,32 @@
 <?php
+
+class DataBase
+{
+  private static $conexao = null;
+
+  public static function getConexao() {
+    if (self::$conexao == null) {
+      $host = "localhost";
+      $nomeBanco = "financas_pessoais";
+      $user = "root";
+      $password = "";
+
+      try {
+        self::$conexao = new PDO(
+          "mysql:host=$host;
+          dbname=$nomeBanco",
+          $user,
+          $password
+        );
+        self::$conexao-> setAttribute(
+          PDO::ATTR_ERRMODE,
+          PDO::ERRMODE_EXCEPTION
+        );
+
+      } catch (PDOException $e) {
+        echo "Erro de conexão: " . $e -> getMessage();
+      }
+    }
+    return self::$conexao;
+  }
+}
--- models/FinanceiroModel.php
+++ models/FinanceiroModel.php
@@ -1,1 +1,33 @@
 <?php
+
+class FinanceiroController
+{
+  private $db;
+
+  public function __construct()
+  {
+    $this->db = DataBase::getConexao();
+  }
+
+  public function getAll() {
+    $consulta = $this->db-> query("SELECT * FROM financeiro_pessoal ORDER BY data");
+    return $consulta->fetchAll(PDO::FETCH_ASSOC);
+  }
+
+  public function getById($id_financeiro) {
+    $sql = $this->db->prepare("SELECT * FROM financeiro_pessoal WHERE id_financeiro = ?");
+    $sql->execute([$id_financeiro]);
+    return $sql->fetch(PDO::FETCH_ASSOC);
+  }
+
+  public function insert($data, $descricao, $valor, $deb_cred, $status) {
+    $sql = $this->db->prepare("INSERT INTO financeiro_pessoal (data, descricao, valor, deb_cred, status) VALUES (?, ?, ?, ?, ?)");
+    return $sql->execute([$data, $descricao, $valor, $deb_cred, $status]);
+  }
+
+  public function cancelar($id_financeiro, $status) {
+    $sql = $this->db->prepare("UPDATE financeiro_pessoal SET status = ? WHERE id_financeiro = ?");
+    return $sql-> execute([$id_financeiro, $status]);
+  }
+
+}
