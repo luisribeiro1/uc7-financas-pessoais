2786b9ab53d80f2edbd74a1c916c5cfbf2765ab0
GUILHERME HENRIQUE SAMPAIO <guilherme.hsampaio@senacsp.edu.br>
2024-10-31 21:25:31 -0300

    FinancasPessoais - 31/10/2024 - Etapa 3

---
+++ documentacao/banco-de-dados.sql
@@ -0,0 +1,9 @@
+-- CREATE TABLE financeiro_pessoal (
+--     id_financeiro INT AUTO_INCREMENT PRIMARY KEY,
+--     data DATE NOT NULL,
+--     descricao VARCHAR(255) NOT NULL,
+--     valor DECIMAL(10, 2) NOT NULL,
+--     deb_cred VARCHAR(10) NOT NULL,
+--     status VARCHAR(20) NOT NULL
+-- );
+
