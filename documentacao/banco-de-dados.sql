4eb4bd65cc6a3496943555d76cbfa7caf382edb8
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-10-31 22:01:06 -0300

    FinancasPessoais - 31/10/2024 - Etapa 3

---
+++ documentacao/banco-de-dados.sql
@@ -0,0 +1,15 @@
+-- Criar o banco de dados
+CREATE DATABASE financas_pessoais;
+
+-- Usar o banco de dados criado
+USE financas_pessoais;
+
+-- Criar a tabela financeiro_pessoal
+CREATE TABLE financeiro_pessoal(
+    id_financeiro INT AUTO_INCREMENT PRIMARY KEY,
+    data DATE NOT NULL,
+    descricao VARCHAR(255),
+    valor DECIMAL(10,2),
+    deb_cred VARCHAR(10),
+    status VARCHAR(20)
+    );
