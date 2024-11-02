6e345dbf8b99c6263e5bead2608aa6be4a3beb84
Luis Henrique <luis.h.souza@outlook.com.br>
2024-11-02 12:14:05 -0300

    :lipstick: feat: Estilização CSS da tabela

---
+++ views/js/index.js
@@ -0,0 +1,7 @@
+require('./style.css');
+
+import lottie from 'lottie-web';
+import { defineElement } from '@lordicon/element';
+
+// define "lord-icon" custom element with default properties
+defineElement(lottie.loadAnimation);
