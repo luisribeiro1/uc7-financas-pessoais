9beb47502690ca14b36a0bdaada459877d4d112d
Luis Henrique <luis.h.souza@outlook.com.br>
2024-11-03 18:59:28 -0300

    feat: Finanças Pessoais: Estilização do CSS e criação de aside form

--- views/js/index.js
+++ views/js/index.js
@@ -5,7 +5,6 @@ import { defineElement } from '@lordicon/element';
 
 // define "lord-icon" custom element with default properties
 defineElement(lottie.loadAnimation);
-<<<<<<< HEAD
 
 // script da aba lateral
 const button = document.getElementById('btn-active');
@@ -27,5 +26,25 @@ toats.addEventListener('click', () => {
 closeButton.addEventListener('click', () => {
   navigationMenu.classList.add('hide');
 });
-=======
->>>>>>> 6e345db (:lipstick: feat: Estilização CSS da tabela)
+
+
+// script da aba lateral
+const button = document.getElementById('btn-active');
+const closeButton = document.querySelector('.close-button');
+const navigationMenu = document.querySelector('.navigation__menu');
+const notification = document.getElementById('notification');
+const toats = document.getElementById('toats');
+
+
+button.addEventListener('click', () => {
+  navigationMenu.classList.remove('none');
+  navigationMenu.classList.remove('hide');
+});
+
+toats.addEventListener('click', () => {
+  notification.classList.remove('none');
+});
+
+closeButton.addEventListener('click', () => {
+  navigationMenu.classList.add('hide');
+});
