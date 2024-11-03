bde9d1f1c9d69d0bbefb04cbb9c01754cc753d6d
Luis Henrique <luis.h.souza@outlook.com.br>
2024-11-03 18:59:28 -0300

    feat: Finanças Pessoais: Estilização do CSS e criação de aside form

--- views/js/index.js
+++ views/js/index.js
@@ -5,3 +5,24 @@ import { defineElement } from '@lordicon/element';
 
 // define "lord-icon" custom element with default properties
 defineElement(lottie.loadAnimation);
+
+// script da aba lateral
+const button = document.getElementById('btn-active');
+const closeButton = document.querySelector('.close-button');
+const navigationMenu = document.querySelector('.navigation__menu');
+const notification = document.getElementById('notification');
+const toats = document.getElementById('toats');
+
+
+button.addEventListener('click', () => {
+  navigationMenu.classList.remove('none');
+  navigationMenu.classList.remove('hide');
+});
+
+toats.addEventListener('click', () => {
+  notification.classList.remove('none');
+});
+
+closeButton.addEventListener('click', () => {
+  navigationMenu.classList.add('hide');
+});
